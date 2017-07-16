c8524571601f2b919be735686829b2998d1da14d
EdAllenPoe <eric@ericgott.com>
2017-07-16 16:11:11 -0700

    While you wait commit

---
+++ WhileYouWait.js
@@ -0,0 +1,35 @@
+var num = 1;                                         // A
+while (num < 6)                                      // B
+{
+  console.log("I'm counting! The number is " + num); // C
+  num = num + 1;                                     // D
+}
+          // E
+
+
+
+
+var days = 60;
+
+while (days > 4){
+
+  console.log(days + " days until my birthday. Such a long time... :(" )
+
+  days= days -1;
+}
+
+while (days > 1){
+
+  console.log(days + " DAYS UNTIL MY BIRTHDAY!!!" )
+
+  days= days -1;
+}
+
+while (days > 0){
+
+  console.log(days + " DAY UNTIL MY BIRTHDAY!!!" )
+
+  days= days -1;
+}
+
+console.log("HAPPY BIRTHDAY PARTY TIME!!!");
