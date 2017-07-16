b8b6558866005deae626e1c6862831156788e1af
EdAllenPoe <eric@ericgott.com>
2017-07-16 14:26:04 -0700

    JS File Basic

---
+++ number.js
@@ -0,0 +1,18 @@
+
+var magic_number=8;
+var super_number=22;
+var mega_number=74;
+var grand_total=42;
+console.log(magic_number,super_number,mega_number,grand_total);
+
+var shoe_string="don't trip";
+var super_string="to much to comprehend";
+var g_string="not enough";
+var string_bean="gross";
+console.log(shoe_string,super_string,g_string,string_bean);
+
+var beyonce= false;
+var misfits= true;
+console.log(beyonce,misfits);
+
+console.log(what_the_hell_is_this_variable);
