557d67beea846ece03763d5cc3860ac9a429dc25
EdAllenPoe <eric@ericgott.com>
2017-07-18 19:25:42 -0700

    Fancy String Take One

---
+++ FancyString.js
@@ -0,0 +1,24 @@
+// var num=[1,2,3,4,5];
+//
+// for (var i=0; i < num.length; i++){
+//
+//   console.log(num[i]);
+// }
+
+
+var names=["Eric","James","Gott","Tom","Bob"];
+
+function fancystring(fancy,names) {
+
+  for (var i=0; i < names.length; i++){
+
+
+  console.log(i,fancy,names[i])
+}
+
+
+}
+
+
+
+fancystring("->",names)
