afe3d9f549326f641c1a4158ce1e7c0ba2b66c1a
EdAllenPoe <eric@ericgott.com>
2017-07-19 17:54:14 -0700

    Random Chance Take One

---
+++ RandomChance.js
@@ -0,0 +1,36 @@
+function quarter(quarters) {
+
+var random=Math.random()*100;
+random=(Math.trunc(random));
+
+while (quarters > 0){
+
+  num=Math.random()*100;
+
+  num=(Math.trunc(num))
+
+  console.log("Your Number =",num);
+  console.log("My Number=",random);
+
+    if(num===random) {
+
+      quarters=(quarters + (Math.random()*50 +51));
+
+    }
+
+    else {console.log("You lose 1 quarter");
+
+    quarters--;}
+
+    console.log(quarters+ " quarters left");
+
+}
+
+console.log("Loser!! I just took your quarters!!  HAHA!!")
+
+
+}
+
+quarter(10)
+
+// put a random chance into the function
